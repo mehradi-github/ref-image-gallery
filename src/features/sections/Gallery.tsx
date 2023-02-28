7493198a7d06dba9545cfb530742b4e26a6310cc
Mehradi <mehradi.info@gmail.com>
2023-02-28 18:13:22 +0100

    Showing images in Gallery

--- src/features/sections/Gallery.tsx
+++ src/features/sections/Gallery.tsx
@@ -5,7 +5,9 @@ const Gallery: FC = () => {
   return (
     <section className="gallery">
       <h1 className="gallery-title">Image Gallery</h1>
-      <Card id={237} />
+      {[237, 42, 76, 206].map((i) => (
+        <Card id={i} />
+      ))}
     </section>
   );
 };
