6f26057f76309be2ac7729c33bcdd9aa96e7416a
Mehradi <mehradi.info@gmail.com>
2023-02-28 18:01:23 +0100

    Adding Card and Gallery commponents

---
+++ src/features/sections/Card.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+interface Iprop {}
+const Card: FC<Iprop> = ({}) => {
+  return <div>Card</div>;
+};
+export default Card;
---
+++ src/features/sections/Gallery.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+const Gallery: FC = () => {
+  return (
+    <section className="gallery">
+      <h1 className="gallery-title">Polaroid Image Gallery</h1>
+    </section>
+  );
+};
+export default Gallery;
